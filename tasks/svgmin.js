9e1f5c45d1e961a70e1765f579f91c917f779fff
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-09 18:55:11 +0100

    refactor

--- tasks/svgmin.js
+++ tasks/svgmin.js
@@ -1,29 +1,27 @@
 'use strict';
 var chalk = require('chalk');
 var eachAsync = require('each-async');
+var prettyBytes = require('pretty-bytes');
+var SVGO = require('svgo');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('svgmin', 'Minify SVG', function () {
-		var options = this.options();
-		var svgo = new (require('svgo'))(options);
-		var filesize = require('filesize');
+		var svgo = new SVGO(this.options());
 
-		eachAsync(this.files, function (el, index, next) {
-			var svgin = grunt.file.read(el.src + '');
-			svgo.optimize(svgin, function (result) {
+		eachAsync(this.files, function (el, i, next) {
+			var srcPath = el.src[0];
+			var srcSvg = grunt.file.read(srcPath);
+
+			svgo.optimize(srcSvg, function (result) {
 				if (result.error) {
-					grunt.warn('Error parsing svg: ' + result.error);
-				} else {
-					var saved = svgin.length - result.data.length;
-					var percentage = saved / svgin.length * 100;
-					var savedFormatted = filesize(saved, {
-						round: 1,
-						spacer: ''
-					});
-
-					grunt.log.writeln(chalk.green('✔ ') + el.src + chalk.gray(' (saved', savedFormatted, Math.round(percentage) + '%)'));
-					grunt.file.write(el.dest, result.data);
+					return grunt.warn('Error parsing SVG: ' + result.error);
 				}
+
+				var saved = srcSvg.length - result.data.length;
+				var percentage = saved / srcSvg.length * 100;
+
+				grunt.log.writeln(chalk.green('✔ ') + srcPath + chalk.gray(' (saved ' + chalk.bold(prettyBytes(saved)) + ' ' + Math.round(percentage) + '%)'));
+				grunt.file.write(el.dest, result.data);
 				next();
 			});
 		}, this.async());
