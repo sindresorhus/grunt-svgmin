9436b2a45ea2c51a6b7441f0b35e46be4fc5c9c5
s.sharov <s.sharov@zebra-group.ru>
2015-06-22 14:29:45 +0300

    Added disablePluginsByDefault option which turns off all plugins that hasn't be enabled by configuration.

--- tasks/svgmin.js
+++ tasks/svgmin.js
@@ -6,9 +6,30 @@ var logSymbols = require('log-symbols');
 var SVGO = require('svgo');
 
 module.exports = function (grunt) {
+
+	var deactivateUnusedPlugins = function (options, svgo) {
+		var active;
+		svgo.config.plugins.forEach(function (svgoPlugins) {
+			svgoPlugins.forEach(function (svgoPlugin) {
+				active = false;
+				options.plugins.forEach(function (optPlugin) {
+					if (optPlugin.hasOwnProperty(svgoPlugin.name)) {
+						active = true;
+					}
+				});
+				if (!active) {
+					svgoPlugin.active = false;
+				}
+			});
+		});
+	};
+
 	grunt.registerMultiTask('svgmin', 'Minify SVG', function () {
 		var done = this.async();
 		var svgo = new SVGO(this.options());
+		if (this.options().disablePluginsByDefault) {
+			deactivateUnusedPlugins(this.options(), svgo);
+		}
 		var totalSaved = 0;
 
 		eachAsync(this.files, function (el, i, next) {
